ffba8363cd27bd5b3d8c241529c0222db3e60f16
Egor <itpd2010@yandex.ru>
2024-01-29 15:03:07 +0100

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.resolve(
+    __dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  const fileStream = fs.createReadStream(filePath);
+
+  const sha256Hash = crypto.createHash("sha256");
+
+  return new Promise((resolve, reject) => {
+    fileStream.on("data", (chunk) => {
+      sha256Hash.update(chunk);
+    });
+
+    fileStream.on("end", () => {
+      const hashResult = sha256Hash.digest("hex");
+      console.log("SHA256 Hash:", hashResult);
+      resolve(hashResult);
+    });
+
+    fileStream.on("error", (error) => {
+      reject(error);
+    });
+  });
 };
 
-await calculateHash();
+calculateHash();
