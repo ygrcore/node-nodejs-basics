daa13924cce13481c79939bb0601dbf28d551ad0
Egor <itpd2010@yandex.ru>
2024-01-29 21:26:00 +0100

    feat: cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+const path = require('path');
+const { spawn } = require('child_process');
+
+const workerPath = path.resolve(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', [workerPath, ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.on('close', () => {
+    process.exit();
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([0, 1, 2, 3, 9]);
+
