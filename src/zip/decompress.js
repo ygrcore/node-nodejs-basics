b3f12504921f84da6aea47d0a339bce4703c3976
Egor <itpd2010@yandex.ru>
2024-01-29 18:19:34 +0100

    feat: zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFilePath = path.resolve(__dirname, 'files', 'archive.gz');
+  const decompressedFilePath = path.resolve(__dirname, 'files', 'fileToCompressBack.txt');
+
+  const compressedStream = fs.createReadStream(sourceFilePath);
+
+  const decompressedStream = fs.createWriteStream(decompressedFilePath);
+
+  const gunzipStream = zlib.createGunzip();
+
+  compressedStream.pipe(gunzipStream).pipe(decompressedStream);
+
+  return new Promise((resolve, reject) => {
+    decompressedStream.on('finish', () => {
+      console.log('File decompress successfully finished');
+      resolve();
+    });
+
+    decompressedStream.on('error', (error) => {
+      reject(error);
+    });
+  });
 };
 
-await decompress();
+decompress();
