2a52f6bab79c7d6713c1fdbe7b0c3c4a4c720456
Egor <itpd2010@yandex.ru>
2024-01-29 13:56:05 +0100

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
-};
+const fs = require('fs').promises;
+const path = require('path');
 
-await read();
+const read = async () => {
+    const fileName = 'fileToRead.txt';
+    const filePath = path.resolve(__dirname, 'files', fileName);
+
+    try {
+      const data = await fs.readFile(filePath, { encoding: 'utf-8' });
+      console.log(data);
+    } catch (err) {
+      throw new Error('FS operation failed: ' + err.message);
+    }
+  };
+
+read();
