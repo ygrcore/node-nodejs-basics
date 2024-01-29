f02b4324a32b907e89bf9edc0b1d40f94452af47
Egor <itpd2010@yandex.ru>
2024-01-29 11:45:42 +0100

    fix: creat.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
 
 const create = async () => {
-    const filePath = path.join(__dirname, 'fresh.txt');
-    fs.writeFile(filePath, 'I am fresh and young', (err) => {
-        if (err) {
-            console.error('FS operation failed', err);
-        } else {
-            console.log('File is written');
-        }
-    });
+  const filePath = path.resolve(__dirname, "fresh.txt");
+  try {
+    await fs.promises.access(filePath);
+    // throw new Error("FS operation failed");
+    console.log('FS operation failed');
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.promises.writeFile(filePath, "I am fresh and young");
+      console.log("File created and written.");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 create();
