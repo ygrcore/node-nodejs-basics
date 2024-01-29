d1c6e41debf58fd5cf1fa0569db63b37ebccbfb9
Egor <itpd2010@yandex.ru>
2024-01-29 13:45:43 +0100

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.resolve(__dirname, 'files');
+
+  try {
+    await fs.access(folderPath);
+
+    const files = await fs.readdir(folderPath);
+
+    console.log('List of files:');
+    files.forEach(file => {
+      console.log(file);
+    });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed: Folder does not exist');
+    } else {
+      console.error('FS operation failed:', err.message);
+    }
+  }
 };
 
-await list();
+list();
