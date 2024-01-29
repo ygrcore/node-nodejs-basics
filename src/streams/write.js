086b7969b0ace9bc8e742c502e017d587d3e0e04
Egor <itpd2010@yandex.ru>
2024-01-29 15:36:26 +0100

    feat: streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
+    const fileStream = fs.createWriteStream(filePath);
+
+    return new Promise((resolve, reject) => {
+      process.stdin.on('data', (chunk) => {
+        fileStream.write(chunk);
+      });
+
+      process.stdin.on('end', () => {
+        fileStream.end();
+        resolve();
+      });
+
+      process.stdin.on('error', (error) => {
+        reject(error);
+      });
+    });
 };
 
-await write();
+write();
