7d6b1417e8dca238b77ae034b26de7b6bb7b7b8b
Egor <itpd2010@yandex.ru>
2024-01-29 16:33:41 +0100

    feat: zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+
 const compress = async () => {
-    // Write your code here 
+  const sourceFilePath = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+  const compressedFilePath = path.resolve(__dirname, 'files', 'archive.gz');
+
+  const sourceStream = fs.createReadStream(sourceFilePath);
+
+  const compressedStream = fs.createWriteStream(compressedFilePath);
+
+  const gzipStream = zlib.createGzip();
+
+  sourceStream.pipe(gzipStream).pipe(compressedStream);
+
+  return new Promise((resolve, reject) => {
+    compressedStream.on('finish', () => {
+      console.log('File compressed successfully finished');
+      resolve();
+    });
+
+    compressedStream.on('error', (error) => {
+      reject(error);
+    });
+  });
 };
 
-await compress();
+compress();
