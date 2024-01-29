cd1314f2f4d7eee00725e824e5d4cc28243a6102
Egor <itpd2010@yandex.ru>
2024-01-29 14:23:43 +0100

    feat: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args.reduce((acc, arg, index, array) => {
+    if (arg.startsWith("--")) {
+      const propName = arg.slice(2);
+      const value = array[index + 1] || null;
+      acc.push(`${propName} is ${value}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
