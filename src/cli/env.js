785a64be37baa68be2d1257d82cf614128612d12
Egor <itpd2010@yandex.ru>
2024-01-29 14:14:04 +0100

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envKeys = Object.keys(process.env);
+
+    const rssKeys = envKeys.filter(key => key.startsWith('RSS_'));
+
+    const resultString = rssKeys.map(key => `${key}=${process.env[key]}`).join('; ');
+
+    console.log(resultString);
 };
 
 parseEnv();
